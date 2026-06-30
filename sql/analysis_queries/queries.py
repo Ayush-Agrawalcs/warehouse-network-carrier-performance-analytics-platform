c79f0613382ea80ab8b8098488387f82ce81b45b
Sakshi Maholiya <apurvamaholiya@gmail.com>
2026-06-30 16:49:51 +0530

    [Sakshi Maholiya] Find top 10 carriers

--- sql/analysis_queries/queries.py
+++ sql/analysis_queries/queries.py
@@ -9,3 +9,15 @@ class Querries:
 
     def warehouses_above_90(self, warehouse_health):
         return warehouse_health.filter(col("utilization_pct") > 90)
+
+     def top_10_carriers(self, final_df):
+        return (
+            final_df
+            .groupBy("carrier")
+            .agg(
+                avg("tpt_day_cnt").alias("avg_transit_days"),
+                count("order_id").alias("order_count")
+            )
+            .orderBy(col("avg_transit_days").asc())
+            .limit(10)
+        )
